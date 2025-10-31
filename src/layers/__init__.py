399b8ba13c1ed02a17d66920afd80383b7827362
Ignacio Peis <ipeaz@dtu.dk>
2025-10-31 15:21:42 +0100

    partial VAE from EDDI

--- src/layers/__init__.py
+++ src/layers/__init__.py
@@ -1,3 +1,5 @@
 from src.layers.conv import *
 from src.layers.resnet import *
 from src.layers.mlp import *
+from src.layers.pn_encoder import *
+from src.layers.pnp_encoder import *
---
+++ src/layers/pnp_encoder.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+from src.layers.mlp import MLP
+
+class PNPEncoder(nn.Module):
+    """
+    Implements the PNP Encoder for handling missing data in a Partial VAE (PVAE) model.
+    https://arxiv.org/pdf/1809.11142
+    """
+    def __init__(self,
+                 input_dim,
+                 emb_dim=10,
+                 h_dim=20,
+                 hidden_dims=[100, 50, 20],
+                 latent_dim=20
+                 ):
+        super(PNPEncoder, self).__init__()
+        self.embs = nn.Parameter(torch.randn(input_dim, emb_dim))
+        self.h = nn.Sequential(
+            MLP(input_dim=emb_dim, hidden_dims=[], output_dim=h_dim),
+            nn.ReLU()
+        )
+
+        self.encoder = MLP(
+            input_dim=h_dim,
+            hidden_dims=hidden_dims,
+            output_dim=2*latent_dim,
+        )
+  
+    def forward(self, x, mask):
+        """
+        x: [batch, input_dim]
+        mask: [batch, input_dim]
+        Returns:
+            
+        """
+        # Enforce x to be (bs, input_dim) shape
+        x = x.view(x.size(0), -1)
+        mask = mask.view(mask.size(0), -1)
+
+        # Expand input x to match embedding dimensions for broadcasting
+        x_exp = x.unsqueeze(-1)                 # [B, D, 1]
+        e_exp = self.embs.unsqueeze(0)                  # [1, D, emb_dim]
+
+        # Multiply x_d * e_d (element-wise multiplication across embedding dim)
+        s = x_exp * e_exp                       # [B, D, emb_dim]
+
+        # Mask out unobserved values
+        mask_exp = mask.unsqueeze(-1)           # [B, D, 1]
+        s = s * mask_exp                        # [B, D, emb_dim]
+
+        # Forward pass through the MLP
+        h = self.h(s.view(-1, s.size(-1)))                           # [B*D, h_dim]
+
+        # Sum across the input dimension
+        h = h.view(x.size(0), -1, h.size(-1)).sum(dim=1)  # [B, h_dim]
+
+        # Encode to mean and logvar
+        mean_logvar = self.encoder(h)  # [B, 2*latent_dim]
+        mean, logvar = torch.chunk(mean_logvar, 2, dim=-1)
+        return mean, logvar
+
+
+
+
+
+
